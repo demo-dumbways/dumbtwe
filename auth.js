54aedd43c240cc1cc4d7fe20d91de6856881a432
= <=>
2022-05-18 10:54:16 +0700

    Day 18

---
+++ auth.js
@@ -0,0 +1,45 @@
+const API_KEY = '4d72be8d-f73a-406d-a9b8-303556d65731'
+
+const kontenbaseClient = new kontenbase.KontenbaseClient({
+    apiKey: API_KEY
+})
+
+async function register() {
+
+    let name = document.getElementById("input-name").value
+    let email = document.getElementById("input-email").value
+    let username = document.getElementById("input-username").value
+    let password = document.getElementById("input-password").value
+
+    const { user, token, error } = await kontenbaseClient.auth.register({
+        firstName: name,
+        email: email,
+        username: username,
+        password: password,
+    })
+
+    window.location.href = "login.html"
+
+}
+
+async function login() {
+
+    let email = document.getElementById("input-email").value
+    let password = document.getElementById("input-password").value
+
+    const { user, token, error } = await kontenbaseClient.auth.login({
+        email: email,
+        password: password,
+    })
+
+    window.location.href = "beranda.html"
+
+}
+
+async function logout() {
+
+    const { user, error } = await kontenbaseClient.auth.logout()
+
+    window.location.href = "index.html"
+
+}
