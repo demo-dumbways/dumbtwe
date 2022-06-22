d6eef21917d31b7081eecb73447e24719d004dab
= <=>
2022-06-22 14:15:22 +0700

    update

---
+++ assets/js/verification.js
@@ -0,0 +1,18 @@
+const API_KEY = 'aea64958-96f0-45dc-b3cc-e9991afd890e'
+
+const kontenbaseClient = new kontenbase.KontenbaseClient({
+    apiKey: API_KEY
+})
+
+
+async function renderThreads() {
+
+}
+
+
+async function addVerification() {
+    let firstName = document.getElementById("fullname").value
+    let influence = document.getElementById("influence").value
+    let idCard = document.getElementById("idCard").value
+    let address = document.getElementById("address").value
+}
